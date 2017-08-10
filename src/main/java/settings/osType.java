cdf2acdfea61534f92fdd76469f71deb48af302e
gil <gil@mbp-de-gil-2.home>
2017-08-10 15:51:14 +0200

    Current operating system identified from an enum

---
+++ src/main/java/settings/osType.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Gil THOMAS
+ * This file forms an integral part of Logfly project
+ * See the LICENSE file distributed with source code
+ * for details of Logfly licence project
+ */
+package settings;
+
+/**
+ *
+ * @author gil
+ */
+public enum osType {WINDOWS, MACOS, LINUX, UNDEFINED}
