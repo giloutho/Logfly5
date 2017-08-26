3e84be1f7f5576a4e20b50cf17cd9a259715c15c
gil <gil@mbp-de-gil-2.home>
2017-08-26 18:38:27 +0200

    Modified

--- src/test/java/gps/flytec20Test.java
+++ src/test/java/gps/flytec20Test.java
@@ -36,14 +36,14 @@ public class flytec20Test {
     @Test
     public void testInit() {
         // Mac OS test        
-        String currNamePort = "/dev/cu.usbserial";
-        try {
-            flytec20 fls = new flytec20();
-            assertTrue(fls.init(currNamePort));
-            System.out.println("Ini test : "+fls.getDeviceType()+" serial "+fls.getDeviceSerial());
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }        
+//        String currNamePort = "/dev/cu.usbserial";
+//        try {
+//            flytec20 fls = new flytec20();
+//            assertTrue(fls.init(currNamePort));
+//            System.out.println("Ini test : "+fls.getDeviceType()+" serial "+fls.getDeviceSerial());
+//        } catch (Exception e) {
+//            System.out.println(e.toString());
+//        }        
     }
 
     @Test
@@ -53,21 +53,21 @@ public class flytec20Test {
     @Test
     public void testGetListFlights() {
         // Mac OS test
-        String currNamePort = "/dev/cu.usbserial";
-        try {
-            flytec20 fls = new flytec20();
-            if (fls.init(currNamePort)) {                    
-                dataImport = FXCollections.observableArrayList();  
-                fls.getListFlights(dataImport);
-                assertTrue(dataImport.size() > 0);     
-                System.out.println("Flights list");
-                for (Gpsmodel nbItem : dataImport){                 
-                    System.out.println(nbItem.getDate()+" "+nbItem.getHeure()+" "+nbItem.getCol4());
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }        
+//        String currNamePort = "/dev/cu.usbserial";
+//        try {
+//            flytec20 fls = new flytec20();
+//            if (fls.init(currNamePort)) {                    
+//                dataImport = FXCollections.observableArrayList();  
+//                fls.getListFlights(dataImport);
+//                assertTrue(dataImport.size() > 0);     
+//                System.out.println("Flights list");
+//                for (Gpsmodel nbItem : dataImport){                 
+//                    System.out.println(nbItem.getDate()+" "+nbItem.getHeure()+" "+nbItem.getCol4());
+//                }
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.toString());
+//        }        
     }
 
     @Test
