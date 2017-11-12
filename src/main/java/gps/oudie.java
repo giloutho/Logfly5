e183686d5a5dcb629c974a81ac2b3db288b15328
gil <gil@mbp-de-gil-2.home>
2017-11-12 22:52:11 +0100

    Naviter Oudie GPS support

---
+++ src/main/java/gps/oudie.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gps;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import settings.osType;
+import systemio.textio;
+
+
+public class oudie {
+    
+    private boolean connected = false;
+    private File fFlights;   
+    private File fDrive;
+    private int idxDrive;    
+    private ObservableList <String> driveList;     
+    private String closingDate;
+    private String msgClosingDate;    
+    
+    public boolean isConnected() {
+        return connected;
+    }
+
+    private void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+
+    public File getfFlights() {
+        return fFlights;
+    }
+    
+    public ObservableList<String> getDriveList() {
+        return driveList;
+    }
+    
+    public String getMsgClosingDate() {
+        return msgClosingDate;
+    }        
+
+    public oudie(osType currOs, int gpsLimit) {
+        boolean conn = false;
+        fFlights = null;        
+        fDrive = null;   
+        
+        setDateLevel(gpsLimit);
+        
+        conn = testConnection(currOs);
+        
+        setConnected(conn);                
+        
+    }    
+    
+    
+        private void setDateLevel(int gpsLimit) {
+        
+        Calendar myCalendar = Calendar.getInstance();
+        myCalendar.add(Calendar.MONTH, -(gpsLimit));
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        closingDate = sdf.format(myCalendar.getTime());   
+        SimpleDateFormat sdfMsg = new SimpleDateFormat("dd/MM/YY");
+        msgClosingDate = sdfMsg.format(myCalendar.getTime());  
+    }
+        
+    public boolean testConnection(osType currOs) {
+        
+        boolean res = false;
+        boolean cond1 = false;
+        boolean cond2 = false;
+        File[] drives = null;
+        int nbDrive = 0;
+            
+        switch (currOs) {
+            case WINDOWS:
+                drives = File.listRoots();
+                break;
+            case MACOS :
+                drives = new File("/Volumes").listFiles();
+                break;
+            case LINUX :
+                File fmedia = new File("/media");
+                for(File fls : fmedia.listFiles()){
+                    // first pass : /media/user
+                    drives = fls.listFiles();                    
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
+        if (drives != null && drives.length > 0) {
+            driveList = FXCollections.observableArrayList();
+            driveList.clear();
+            for (File aDrive : drives) {       
+                long size = aDrive.getTotalSpace();
+                float sizeGo = size / 1000000000;    
+                StringBuilder sb = new StringBuilder();
+                sb.append(aDrive.getName()).append(" ").append(String.format("%4.0f", sizeGo)).append(" Go");
+                driveList.add(sb.toString());
+                // Capacity > 64 Go jumped
+                if (size < 63999999999L) {
+                    File listFile[] = aDrive.listFiles();
+                    if (listFile != null) {
+                        for (int i=0; i<listFile.length; i++)         
+                        {
+                            if (listFile[i].getName().equals("Settings") && listFile[i].isDirectory()) {
+                                try {
+                                    if (exploreFolderSettings(listFile[i])) cond1 = true;
+                                } catch (Exception e) {
+                                    
+                                }
+                            }
+                            if (listFile[i].getName().equals("Flights") && listFile[i].isDirectory()) {
+                                fFlights = listFile[i];
+                                cond2 = true;
+                            }
+                        }
+                        if (cond1 == true && cond2 == true) {
+                            fDrive = aDrive;
+                            idxDrive = nbDrive;
+                            res = true;
+                            break;
+                        } else {
+                            idxDrive = 0;
+                        }
+                    }
+                }
+                nbDrive++;
+            }
+        }                                    
+        return res;                
+    } 
+    
+    /** 
+     * SD card of Skytraax has a folder called Settings with a default settings file called Default.xml 
+     * @param dir
+     * @return
+     * @throws Exception 
+     */
+    private boolean exploreFolderSettings(File dir) throws Exception {  
+               
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i].getName();
+            if (fileName.equals("Default.xml")) {                                    
+                return true;
+            }
+        } 
+        
+        return false;
+    }       
+    
+    private void exploreFolder(File dir, ArrayList<String> trackPathList) throws Exception {  
+        // Recursivité à vérifier pour le skytraax        
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i].getName();
+            if (fileName.endsWith(".igc") || fileName.endsWith(".IGC")) {                                    
+                // Problem of dot files writed by MacOS 
+                if (files[i].isFile() && !fileName.startsWith("._") && files[i].getName().length() > 9) {
+                    if (files[i].getName().substring(0,10).compareTo(closingDate) > 0) {
+                        trackPathList.add(files[i].getPath());
+                        System.out.println("    add : "+files[i].getPath());
+                    }
+                }
+            }
+            if (files[i].isDirectory()) {
+                exploreFolder(files[i], trackPathList);
+            }
+        }        
+    }    
+    
+    public void listTracksFiles(ArrayList<String> trackPathList) throws Exception {   
+
+        if (fFlights != null && fFlights.exists())  {        
+           exploreFolder(fFlights, trackPathList);
+        }
+    }
+    
+    public String getTrackFile(String igcPath) {
+        
+        String res = null;
+        
+        File fIGC = new File(igcPath);
+        textio fread = new textio();                                    
+        String pFichier = fread.readTxt(fIGC);
+        if (pFichier != null && !pFichier.isEmpty())  {
+            res = pFichier;
+        }
+        
+        return res;        
+    }    
+    
+}
