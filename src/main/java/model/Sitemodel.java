bb86618c862072e11f7a9be3b4d4008bdc2908a0
gil <gil@mbp-de-gil-2.home>
2018-01-21 23:39:18 +0100

    Initialization

---
+++ src/main/java/model/Sitemodel.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright Gil THOMAS
+ * This file forms an integral part of Logfly project
+ * See the LICENSE file distributed with source code
+ * for details of Logfly licence project
+ */
+package model;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author gil
+ */
+public class Sitemodel {
+    
+    public SimpleStringProperty idSite = new SimpleStringProperty();
+    public SimpleStringProperty nom = new SimpleStringProperty();
+    public SimpleStringProperty ville = new SimpleStringProperty();
+    public SimpleStringProperty cp = new SimpleStringProperty();    
+    public SimpleStringProperty alt = new SimpleStringProperty();
+    public SimpleStringProperty orient = new SimpleStringProperty();    
+    public SimpleStringProperty type = new SimpleStringProperty();
+     
+    public String getIdSite() {
+        return idSite.get();
+    }
+       
+    public void setIdSite(String idVolStr) {    
+        idSite.set(idVolStr);
+    }
+
+    public String getNom() {
+        return nom.get();
+    }
+
+    public void setNom(String nomStr) {
+        nom.set(nomStr);
+    }
+    
+    public String getVille() {
+        return ville.get();
+    }
+
+    public void setVille(String villeStr) {
+        ville.set(villeStr);
+    }    
+    
+    public String getCp() {
+        return cp.get();
+    }
+
+    public void setCp(String cpStr) {
+        cp.set(cpStr);
+    }  
+    
+    public String getAlt() {
+        return alt.get();
+    }
+
+    public void setAlt(String altStr) {
+        alt.set(altStr);
+    }  
+    
+    public String getOrient() {
+        return orient.get();
+    }
+
+    public void setOrient(String orientStr) {
+        orient.set(orientStr);
+    }         
+    
+    public String getType() {
+        return type.get();
+    }
+
+    public void setType(String typeStr) {
+        type.set(typeStr);
+    }    
+    
+}
