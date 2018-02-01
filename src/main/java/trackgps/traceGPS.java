907009fb342882638b063a144bdf9585bfc89bd4
gil <gil@mbp-de-gil-2.home>
2018-02-01 18:37:03 +0100

    Name and glider decoding characters corrected

--- src/main/java/trackgps/traceGPS.java
+++ src/main/java/trackgps/traceGPS.java
@@ -706,7 +706,7 @@ public class traceGPS {
                                   // we can find a carriage return
                                   for (int kk = 0 ; kk < sPilote.length() ; kk++) {
                                         char aChar = sPilote.charAt(kk);                                      
-                                        if ((int)aChar > 31 && (int)aChar < 122) {
+                                        if ((int)aChar > 31 && (int)aChar < 123) {
                                             if ((int)aChar == 39) GoodName += "\\";     // Escape single quote                                                                                                           
                                             GoodName = GoodName.concat(sPilote.substring(kk,kk+1));
                                         }
@@ -723,7 +723,7 @@ public class traceGPS {
                                   // always a possible CR in the string
                                   for (int kk = 0 ; kk < sVoile.length() ; kk++) {
                                         char aChar = sVoile.charAt(kk);                                      
-                                        if ((int)aChar > 31 && (int)aChar < 122) {
+                                        if ((int)aChar > 31 && (int)aChar < 123) {
                                             if ((int)aChar == 39) GoodName += "\\";     // Escape single quote  
                                             GoodName = GoodName.concat(sVoile.substring(kk,kk+1));
                                         }
@@ -738,7 +738,7 @@ public class traceGPS {
                                   // always a possible CR in the string
                                   for (int kk = 0 ; kk < sVoile.length() ; kk++) {
                                         char aChar = sVoile.charAt(kk);                                      
-                                        if ((int)aChar > 31 && (int)aChar < 122) GoodName = GoodName.concat(sVoile.substring(kk,kk+1));
+                                        if ((int)aChar > 31 && (int)aChar < 123) GoodName = GoodName.concat(sVoile.substring(kk,kk+1));
                                   }
                                   sVoile = GoodName;
                                 break;
