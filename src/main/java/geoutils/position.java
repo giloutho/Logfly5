dff2dc31f413b1898182acc90c0c3106145cb2e5
gil <gil@mbp-de-gil-2.home>
2018-02-04 12:30:07 +0100

    Each value has now a public accesser

--- src/main/java/geoutils/position.java
+++ src/main/java/geoutils/position.java
@@ -29,6 +29,22 @@ public class position {
     private double longSec_ms;    
     private String meridien;
 
+    public position() {
+        altitude = 0;
+//        hemisphere = "N";
+//        latDegres = 0;
+//        latitude =0;
+//        latMin_mm = 0;
+//        latMin_ms = 0;
+//        latSec_ms = 0;
+//        longDegres = 0;
+//        longitude = 0;
+//        longMin_mm = 0;
+//        longMin_ms = 0;
+//        longSec_ms = 0;    
+//        meridien = "E";        
+    }
+    
     public int getAltitude() {
         return altitude;
     }
@@ -41,102 +57,103 @@ public class position {
         return hemisphere;
     }
 
-    public void setHemisphere(String hemiphere) {
-        this.hemisphere = hemiphere;
-    }
-
     public int getLatDegres() {
         return latDegres;
-    }
-
-    public void setLatDegres(int latDegres) {
-        this.latDegres = latDegres;
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLatMin_mm() {
         return latMin_mm;
-    }
-
-    public void setLatMin_mm(double latMin_mm) {
-        this.latMin_mm = latMin_mm;
     }
 
     public int getLatMin_ms() {
         return latMin_ms;
     }
 
-    public void setLatMin_ms(int latMin_ms) {
-        this.latMin_ms = latMin_ms;
-    }
-
     public double getLatSec_ms() {
         return latSec_ms;
-    }
-
-    public void setLatSec_ms(double latSec_ms) {
-        this.latSec_ms = latSec_ms;
     }
 
     public int getLongDegres() {
         return longDegres;
     }
 
-    public void setLongDegres(int longDegres) {
-        this.longDegres = longDegres;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     public double getLongMin_mm() {
         return longMin_mm;
     }
 
-    public void setLongMin_mm(double longMin_mm) {
-        this.longMin_mm = longMin_mm;
-    }
-
     public int getLongMin_ms() {
         return longMin_ms;
-    }
-
-    public void setLongMin_ms(int longMin_ms) {
-        this.longMin_ms = longMin_ms;
     }
 
     public double getLongSec_ms() {
         return longSec_ms;
     }
 
-    public void setLongSec_ms(double longSec_ms) {
-        this.longSec_ms = longSec_ms;
-    }
-
     public String getMeridien() {
         return meridien;
     }
+    
+    public void setLatDegres(int latDegres) {
+        this.latDegres = latDegres;
+        setLatitudeDMm(latDegres, getLatMin_mm(), getHemisphere());
+       // setLatitudeDMS(latDegres, getLatMin_ms(), getLatSec_ms(), getHemisphere());
+    }
+        
+    public void setLatMin_mm(double latMin_mm) {
+        this.latMin_mm = latMin_mm;
+        setLatitudeDMm(getLatDegres(), latMin_mm, getHemisphere());
+    }    
+    
+    public void setHemisphere(String hemisphere) {
+        this.hemisphere = hemisphere;
+        setLatitudeDMm(getLatDegres(), getLatMin_mm(), hemisphere);
+    }
+    public void setLatMin_ms(int latMin_ms) {
+        this.latMin_ms = latMin_ms;
+        setLatitudeDMS(getLatDegres(), latMin_ms, getLatSec_ms(), getHemisphere());
+    }   
+    
+    public void setLatSec_ms(double latSec_ms) {
+        this.latSec_ms = latSec_ms;
+        setLatitudeDMS(getLatDegres(), getLatMin_ms(), latSec_ms, getHemisphere());
+    }    
 
-    public void setMeridien(String meridien) {
-        this.meridien = meridien;
+    public void setLongDegres(int longDegres) {
+        this.longDegres = longDegres;
+        setLongitudeDMm(longDegres, getLongMin_mm(), getMeridien());
+     //   setLongitudeDMS(longDegres, getLongMin_mm(), getLongSec_ms(), getMeridien());
+    }   
+    
+    public void setLongMin_mm(double longMin_mm) {
+        this.longMin_mm = longMin_mm;
+        setLongitudeDMm(getLongDegres(), longMin_mm, getMeridien());
     }
     
+    public void setLongMin_ms(int longMin_ms) {
+        this.longMin_ms = longMin_ms;
+        setLongitudeDMS(getLongDegres(), longMin_ms, getLongSec_ms(), getMeridien()); 
+    }    
     
+    public void setLongSec_ms(double longSec_ms) {
+        this.longSec_ms = longSec_ms;
+        setLongitudeDMS(getLongDegres(), getLongMin_ms(), longSec_ms, getMeridien()); 
+    }
+    
+    public void setMeridien(String meridien) {
+        this.meridien = meridien;
+        setLongitudeDMm(getLongDegres(), getLongMin_mm(), meridien);
+    }    
     
     public void setLatitudeDd(double dDecimalDegrees)  {
-        this.setLatitude(dDecimalDegrees);
+        latitude = dDecimalDegrees;
         // from http://forums.esri.com/Thread.asp?c=93&f=993&t=116218
         // converts decimal degrees longitude to DDDMMSSH and decimal degrees latitude to DDMMSSH
   
@@ -150,16 +167,16 @@ public class position {
             iSign = 1;
         }
   
-        this.setLatDegres((int)Math.floor(dDecimalDegrees));
+        latDegres = ((int)Math.floor(dDecimalDegrees));
         dMinutesPart = (dDecimalDegrees - latDegres) * 60;
-        this.setLatMin_mm(dMinutesPart);
-        this.setLatSec_ms((dMinutesPart - Math.floor(dMinutesPart)) * 60);
-        this.setLatMin_ms((int)Math.floor(dMinutesPart));
+        latMin_mm = dMinutesPart;
+        latSec_ms = (dMinutesPart - Math.floor(dMinutesPart)) * 60;
+        latMin_ms = (int)Math.floor(dMinutesPart);
   
         if (iSign == 1)
-            this.setHemisphere("N");
+            hemisphere = "N";
         else
-            this.setHemisphere("S");        
+            hemisphere = "S";        
     }
     
     public void setLatitudeDMm(int pDeg, double pMin, String pHem) {
@@ -168,35 +185,28 @@ public class position {
         calcLatitude = pDeg+((pMin*60)/3600);
         // si hémisphère Sud on passe en négatif
         if (pHem.equals("S")) calcLatitude = calcLatitude * - 1;
-        this.setLatitude(calcLatitude);        
-        this.setLatDegres((int)pDeg);
-        this.setLatMin_ms((int)Math.floor(pMin));
-        this.setLatMin_mm(pMin);
-        this.setLatSec_ms ((pMin- this.getLatMin_ms())*60);
-        this.setHemisphere(pHem);        
-        System.out.println(pDeg+"-"+pMin+"-"+pHem);
-        System.out.println(this.getLatitude());
-        System.out.println(this.getLatDegres());
-        System.out.println(this.getLatMin_ms());
-        System.out.println(this.getLatMin_mm());
-        System.out.println(this.getLatSec_ms());
-        System.out.println(this.getHemisphere());        
+        latitude = calcLatitude;        
+        latDegres = ((int)pDeg);
+        latMin_ms = (int)Math.floor(pMin);
+        latMin_mm = pMin;
+        latSec_ms = (pMin- this.getLatMin_ms())*60;
+        hemisphere = pHem;               
     }
     
     public void setLatitudeDMS(int pDeg, int pMin, double pSec, String pHem) {
         double calcLatitude = pDeg+(((pMin*60)+(pSec))/3600);
         // Si hémisphère Sud on passe en négatif
         if (pHem.equals("S")) calcLatitude = calcLatitude * - 1;
-        this.setLatitude(calcLatitude);  
-        this.setLatDegres((int)pDeg);
-        this.setLatMin_ms((int)pMin);
-        this.setLatMin_mm(pMin+(pSec/60));
-        this.setLatSec_ms(pSec);
-        this.setHemisphere(pHem);         
+        latitude = calcLatitude;  
+        latDegres = ((int)pDeg);
+        latMin_ms = (int)pMin;
+        latMin_mm = pMin+(pSec/60);
+        latSec_ms = pSec;
+        hemisphere = pHem;         
     }
     
-    public void setLongitudedeDd(double dDecimalDegrees)  {
-        this.setLongitude(dDecimalDegrees);
+    public void setLongitudeDd(double dDecimalDegrees)  {
+        longitude = dDecimalDegrees;
         // from http://forums.esri.com/Thread.asp?c=93&f=993&t=116218
         double dMinutesPart;
         int iSign;
@@ -208,16 +218,16 @@ public class position {
             iSign = 1;
         }
   
-        this.setLongDegres((int)Math.floor(dDecimalDegrees));
+        longDegres = ((int)Math.floor(dDecimalDegrees));
         dMinutesPart = (dDecimalDegrees - longDegres) * 60;
-        this.setLongMin_mm(dMinutesPart);
-        this.setLongSec_ms((dMinutesPart - Math.floor(dMinutesPart)) * 60);
-        this.setLongMin_ms((int)Math.floor(dMinutesPart));
+        longMin_mm = dMinutesPart;
+        longSec_ms = (dMinutesPart - Math.floor(dMinutesPart)) * 60;
+        longMin_ms = (int)Math.floor(dMinutesPart);
   
         if (iSign == 1)
-            this.setMeridien("E");
+            meridien = "E";
         else
-            this.setMeridien("W");                
+            meridien = "W";                
     }
     
     public void setLongitudeDMm(int pDeg, double pMin, String pMer)  {
@@ -225,24 +235,24 @@ public class position {
         calcLongitude = pDeg+((pMin*60)/3600);
         // si on est en Ouest on passe en négatif
         if (pMer.equals("W")) calcLongitude = calcLongitude * - 1;
-        this.setLongitude(calcLongitude);        
-        this.setLongDegres((int)pDeg);
-        this.setLongMin_ms((int)Math.floor(pMin));
-        this.setLongMin_mm(pMin);
-        this.setLongSec_ms ((pMin- this.getLongMin_ms())*60);
-        this.setMeridien(pMer);   
+        longitude = calcLongitude;        
+        longDegres = ((int)pDeg);
+        longMin_ms = (int)Math.floor(pMin);
+        longMin_mm = pMin;
+        longSec_ms = (pMin- this.getLongMin_ms())*60;
+        meridien = pMer;   
     }
     
     public void setLongitudeDMS(int pDeg, double pMin, double pSec, String pMer)  {
         double calcLongitude = pDeg+(((pMin*60)+(pSec))/3600);
         // est en Ouest on passe en négatif
         if (pMer.equals("W")) calcLongitude = calcLongitude * - 1;
-        this.setLongitude(calcLongitude);  
-        this.setLongDegres((int)pDeg);
-        this.setLongMin_ms((int)pMin);
-        this.setLongMin_mm(pMin+(pSec/60));
-        this.setLongSec_ms(pSec);
-        this.setMeridien(pMer);          
+        longitude = calcLongitude;  
+        longDegres = ((int)pDeg);
+        longMin_ms = (int)pMin;
+        longMin_mm = pMin+(pSec/60);
+        longSec_ms = pSec;
+        meridien = pMer;          
     }
     
 }
