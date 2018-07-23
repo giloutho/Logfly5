3ea0e5896b16c8ec685e18f25cc91f00571424eb
gil <gil@mbp-de-gil-2.home>
2018-07-23 17:43:35 +0200

    XC Planner saving operations integrated

--- src/main/java/controller/FullMapController.java
+++ src/main/java/controller/FullMapController.java
@@ -8,10 +8,13 @@ package controller;
 
 import dialogues.alertbox;
 import dialogues.dialogbox;
+import geoutils.googlegeo;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.logging.Level;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
@@ -26,7 +29,11 @@ import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import leaflet.map_visu;
 import littlewins.winMail;
+import littlewins.winSaveXcp;
 import littlewins.winTrackFile;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.xnap.commons.i18n.I18n;
 import org.xnap.commons.i18n.I18nFactory;
 import settings.configProg;
@@ -53,6 +60,9 @@ public class FullMapController {
     
     @FXML
     private Button btScoring;    
+    
+    @FXML
+    private Button btXcplanner;
     
     @FXML
     private Button btHtml;    
@@ -126,6 +136,12 @@ public class FullMapController {
         askScoring();
     }   
     
+    // XC planner clic
+    @FXML
+    private void handleXcp(ActionEvent event) {
+        xcpTranslation();
+    }     
+    
     private void askScoring() {
         scoring currScore = new scoring(this,myConfig);  
         currScore.start(mapTrace,chbLeague.getSelectionModel().getSelectedIndex());
@@ -153,6 +169,7 @@ public class FullMapController {
             try {
                 map_visu visuFullMap = new map_visu(mapTrace, myConfig);
                 if (visuFullMap.isMap_OK()) {
+                    btXcplanner.setVisible(true);
                     carnetHTML = visuFullMap.getMap_HTML();
                     viewMap.getEngine().loadContent(carnetHTML,"text/html");
                 }
@@ -171,13 +188,186 @@ public class FullMapController {
         
     }    
     
+    private void xcpTranslation() {
+        String sflightType="";
+        StringBuilder sbUrl = new StringBuilder();
+        StringBuilder sbStart = new StringBuilder();
+        StringBuilder totUrl = new StringBuilder();
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();    
+        decimalFormatSymbols.setDecimalSeparator('.'); 
+        DecimalFormat df2 = new DecimalFormat("#0.00000", decimalFormatSymbols);
+        DecimalFormat df3 = new DecimalFormat("##0.00000", decimalFormatSymbols);
+        String sLat;
+        String googLat;
+        double dLat;
+        String sLong;
+        String googLong;
+        double dLong;
+        boolean isTriangle = false;
+        String sLocation=null;
+        
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(mapTrace.getScore_JSON());
+            JSONObject score = (JSONObject) jsonObject.get("drawScore");
+            System.out.println("Score league " + score.get("scoreLeague")); 
+            String sLeague =  score.get("scoreLeague").toString();
+            String sShape = score.get("scoreShape").toString().trim();
+            String scoreBrut = score.get("scorePoints").toString().trim();
+            String distBrute = score.get("scoreDistance").toString().trim();
+            double dDist = Double.valueOf(distBrute)*1000;   
+            String distMetre = df3.format(dDist) ;
+            switch (sShape) {
+                case "Free flight" :
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd2";
+                            break;
+                        case "XC" :
+                            sflightType = "xc2";
+                            break;                            
+                    }
+                    break;
+                case "Free flight 1 wpt" :
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd3";
+                            break;
+                        case "XC" :
+                            sflightType = "xc3";
+                            break;                            
+                    }                    
+                    break;
+                case "Free flight 2 wpt" :
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd4";
+                            break;
+                        case "XC" :
+                            sflightType = "xc4";
+                            break;                            
+                    }                    
+                    break;
+                case "Free flight 3 wpt" :
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd5";   // à priori ne sortira pas avec points
+                            break;
+                        case "XC" :
+                            sflightType = "xc5";
+                            break;                            
+                    }                    
+                    break;
+                case "outReturn" :
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd2c";
+                            break;
+                        case "XC" :
+                            sflightType = "xc2c";  // à priori ne sortira pas avec points
+                            break;                            
+                    }                    
+                    break;
+                case "FAI Triangle" :
+                    isTriangle = true;
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd3c";
+                            break;
+                        case "XC" :
+                            sflightType = "xc3c";
+                            break;                            
+                    }                          
+                    break;
+                case "Flat Triangle" :
+                    isTriangle = true;
+                    switch (sLeague) {
+                        case "FR":
+                            sflightType = "cfd3c";
+                            break;
+                        case "XC" :
+                            sflightType = "xc3c";
+                            break;                            
+                    }                          
+                    break;                    
+            }
+            sbUrl.append("&flightType=").append(sflightType);
+            JSONArray tabPoints = (JSONArray) jsonObject.get("drawPoints");
+            if (tabPoints != null)  {
+                sbUrl.append("&turnpoints=%5B%5B");                
+                for(int i=0; i<tabPoints.size(); i++){ 
+                    String[] arCoord = tabPoints.get(i).toString().split(",");
+                    if (i > 1) sbUrl.append(",%5B");
+                    if (arCoord.length > 1) {
+                        sLat = arCoord[0].replace("[", "");
+                        // To avoid errors with Google maps API,
+                        // we must limit number of decimals (max 6)
+                        // With points we have currently 9 decimals
+                        dLat = Double.parseDouble(sLat);   
+                        googLat = df2.format(dLat);
+                        sLong = arCoord[1].replace("[", "");
+                        dLong = Double.parseDouble(sLong);   
+                        googLong = df3.format(dLong);
+                        if ( i == tabPoints.size() - 1) {
+                            sbUrl.append(googLat).append(",").append(googLong).append("%5D");  
+                        } else {
+                            if (i==0 ) {
+                                sLocation = askLocationName(dLat,dLong);
+                                if (isTriangle)
+                                    sbStart.append("%5D&start=%5B").append(googLat).append(",").append(googLong).append("%5D");
+                                else
+                                    sbUrl.append(googLat).append(",").append(googLong).append("%5D,%5B");
+                            } else {
+                                sbUrl.append(googLat).append(",").append(googLong).append("%5D");
+                            }
+                        }
+                    }
+                }
+                if (isTriangle) 
+                    sbUrl.append(sbStart.toString());
+                else
+                    sbUrl.append("%5D");
+            } 
+            // unused here only required by XcpViewController
+            totUrl.append("Timestamp=1111111111");
+            totUrl.append("&Distance=").append(distMetre);
+            totUrl.append("&Score=").append(scoreBrut);
+            totUrl.append("&location=").append(sLocation);
+            totUrl.append(sbUrl.toString());
+            System.out.println(totUrl.toString());
+            if (totUrl.toString().contains("turnpoints")) {
+                winSaveXcp saveWin = new winSaveXcp(i18n, totUrl.toString(), "WP");        
+            }
+        } catch (Exception e) {
+            
+        }
+    }
+    
+    private String askLocationName(double dLat, double dLong) {
+        String res = i18n.tr("Inconnu");
+        
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+        decimalFormatSymbols.setDecimalSeparator('.');        
+        DecimalFormat decimalFormat = new DecimalFormat("###.0000", decimalFormatSymbols);
+        String sCoord = decimalFormat.format(dLat)+","+decimalFormat.format(dLong);
+        googlegeo myGoog = new googlegeo();
+        if (myGoog.googleReverseGeo(sCoord) == 0) {
+            res = myGoog.getGeoVille();
+             
+        } 
+        
+        return res;
+    }
+    
     @FXML
     private void sendMail(ActionEvent event) {
+
         if (snapshot()) {            
             winMail showMail = new winMail(myConfig,snapFile.getAbsolutePath(), false);            
         } else {
             winMail showMail = new winMail(myConfig,null, false); 
-        }            
+        }
+        
     }    
     
     @FXML
@@ -267,9 +457,11 @@ public class FullMapController {
                     break;
                 }
             }
+            btXcplanner.setVisible(true);
         } else {
             chbLeague.getSelectionModel().select(myConfig.getIdxLeague()); 
             firstLeagueIdx = -1;
+            btXcplanner.setVisible(false);
         }
     }    
     
