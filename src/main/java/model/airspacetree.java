2a11009d8b92717aca4c69488b2bf5a4b67617fe
gil <gil@mbp-de-gil-2.home>
2018-08-20 18:46:22 +0200

    Model for airspaces in treeview

---
+++ src/main/java/model/airspacetree.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gil
+ */
+public class airspacetree {
+    
+    private String name;  
+    private boolean selected;    
+    
+    public airspacetree(String pName, boolean pSelected) {
+        this.name = pName;
+        this.selected = pSelected;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }        
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
+}
