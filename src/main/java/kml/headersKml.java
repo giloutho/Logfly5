fa9e91bf45795fd13b825d1c5d45ec0ccba35ec7
gil <gil@mbp-de-gil-2.home>
2018-12-30 22:28:58 +0100

    All UI strings translated in english

--- src/main/java/kml/headersKml.java
+++ src/main/java/kml/headersKml.java
@@ -65,46 +65,46 @@ public class headersKml {
 
             // Descriptive Panel
             res.append("     <description><![CDATA[<table cellpadding=\"1\" cellspacing=\"1\">").append(RC);
-            res.append("        <tr bgcolor=\"#dddddd\\\"><th align=\"right\">").append(i18n.tr("Nom Pilote")).append("</th><td>").append(KmlTrace.getsPilote()).append("</td></tr>").append(RC);
+            res.append("        <tr bgcolor=\"#dddddd\\\"><th align=\"right\">").append(i18n.tr("Pilot name")).append("</th><td>").append(KmlTrace.getsPilote()).append("</td></tr>").append(RC);
             res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append("Voile").append("</th><td>").append(KmlTrace.getsVoile()).append("</td></tr>").append(RC);
             String hDeco = KmlTrace.getDT_Deco().format(DateTimeFormatter.ofPattern("HH:mm")); 
-            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Heure Déco")).append("</th><td>").append(hDeco).append("</td></tr>").append(RC);
+            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Take-off time")).append("</th><td>").append(hDeco).append("</td></tr>").append(RC);
             String hAtterro = KmlTrace.getDT_Attero().format(DateTimeFormatter.ofPattern("HH:mm")); 
-            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Heure Attero")).append("</th><td>").append(hAtterro).append("</td></tr>").append(RC);
+            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Landing time")).append("</th><td>").append(hAtterro).append("</td></tr>").append(RC);
             res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append("Durée").append("</th><td>").append(KmlTrace.getsDuree_Vol()).append("</td></tr>").append(RC);
 
             if (currMakingKml.isGraphAltiBaro()) {
-                res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Alt Deco Baro")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Deco_Baro())).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Take-off Baro Alt")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Deco_Baro())).append(" m</td></tr>").append(RC);
                 pointIGC ptAltMax = KmlTrace.getAlt_Maxi_Baro();
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alti Max Baro")).append("</th><td>").append(String.valueOf(ptAltMax.AltiBaro)).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Max Baro Alt")).append("</th><td>").append(String.valueOf(ptAltMax.AltiBaro)).append(" m</td></tr>").append(RC);
                 pointIGC ptAltMini = KmlTrace.getAlt_Mini_Baro();
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alti Mini Baro")).append("</th><td>").append(String.valueOf(ptAltMini.AltiBaro)).append(" m</td></tr>").append(RC);
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alt Attero Baro")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Attero_Baro())).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Min baro alt")).append("</th><td>").append(String.valueOf(ptAltMini.AltiBaro)).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Landing Baro Alt")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Attero_Baro())).append(" m</td></tr>").append(RC);
             }  else {
-                res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Alti Déco GPS")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Deco_GPS())).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Take-off GPS Alt")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Deco_GPS())).append(" m</td></tr>").append(RC);
                 pointIGC ptAltMax = KmlTrace.getAlt_Maxi_GPS();
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alti Max GPS")).append("</th><td>").append(String.valueOf(ptAltMax.AltiGPS)).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Max GPS Alt")).append("</th><td>").append(String.valueOf(ptAltMax.AltiGPS)).append(" m</td></tr>").append(RC);
                 pointIGC ptAltMini = KmlTrace.getAlt_Mini_GPS();
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alti Min GPS")).append("</th><td>").append(String.valueOf(ptAltMini.AltiGPS)).append(" m</td></tr>").append(RC);
-                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Alt Attero GPS")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Attero_GPS())).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Min GPS Alt")).append("</th><td>").append(String.valueOf(ptAltMini.AltiGPS)).append(" m</td></tr>").append(RC);
+                res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Landing GPS Alt")).append("</th><td>").append(String.valueOf(KmlTrace.getAlt_Attero_GPS())).append(" m</td></tr>").append(RC);
             }
 
             // in xLogfly, we must check values to avoid some strings like -NAN(000).00, -INF.00 ou -1.#J 
             // They crashed kml display
             // Normally with String.format, we don't need to check
             // Max gain          
-            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Gain max")).append("</th><td>").append(String.valueOf(KmlTrace.getBestGain())).append("m</td></tr>").append(RC);
+            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Max gain")).append("</th><td>").append(String.valueOf(KmlTrace.getBestGain())).append("m</td></tr>").append(RC);
             // Max vario 
             pointIGC ptVarioMax = KmlTrace.getVario_Max();                    
-            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Vario max")).append("</th><td>").append(String.format("%2.2f",ptVarioMax.Vario)).append(" m/s</td>").append(RC);
+            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Max climb")).append("</th><td>").append(String.format("%2.2f",ptVarioMax.Vario)).append(" m/s</td>").append(RC);
             // Mini vario
             pointIGC ptVarioMini = KmlTrace.getVario_Mini();
-            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Vario mini")).append("</th><td>").append(String.format("%2.2f",ptVarioMini.Vario)).append(" m/s</td></tr>").append(RC);
+            res.append("        <tr bgcolor=\"#ffffff\"><th align=\"right\">").append(i18n.tr("Max sink")).append("</th><td>").append(String.format("%2.2f",ptVarioMini.Vario)).append(" m/s</td></tr>").append(RC);
             // Max speed
             pointIGC ptVitMax = KmlTrace.getVit_Max();        
-            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Vitesse max")).append("</th><td>").append(String.format("%3.2f",ptVitMax.Vitesse)).append(" km/h</td></tr></table>").append(RC);
+            res.append("        <tr bgcolor=\"#dddddd\"><th align=\"right\">").append(i18n.tr("Max speed")).append("</th><td>").append(String.format("%3.2f",ptVitMax.Vitesse)).append(" km/h</td></tr></table>").append(RC);
 
-            res.append("        <br><br><a href='http://www.logfly.org/index.php?id=google-earth-utilisation&lang=fr' target='_blank'>").append(i18n.tr("Aide sur l'exploitation de ce fichier kml")).append("</a>]]>").append(RC);
+            res.append("        <br><br><a href='http://www.logfly.org/index.php?id=google-earth-utilisation&lang=fr' target='_blank'>").append(i18n.tr("Help for exploitation of this kml file")).append("</a>]]>").append(RC);
             res.append("     </description>").append(RC);
             res.append("     <Snippet maxLines=\"1\"> ").append(KmlTrace.getsDate_Vol()).append("  ").append(hDeco).append(" ").append(hAtterro).append("</Snippet>").append(RC);                
             kmlString = res.toString();
