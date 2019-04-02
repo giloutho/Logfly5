3c88e82d3ec9c01ff7b1c4d5cfdfa66e9a33f073
gil <gil@mbp-de-gil-2.home>
2019-04-02 22:28:41 +0200

    Show a local html file in a webviewer

---
+++ src/main/java/littlewins/winWebFile.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Gil THOMAS
+ * This file forms an integral part of Logfly project
+ * See the LICENSE file distributed with source code
+ * for details of Logfly licence project
+ */
+package littlewins;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+import settings.configProg;
+
+/**
+ *
+ * @author gil
+ */
+public class winWebFile {
+    
+    private configProg myConfig;
+    private I18n i18n; 
+    private String htmlCode;
+    private boolean sizeMax;
+    
+    public winWebFile(configProg currConfig, String pHTML, boolean pSizeMax) {
+        // it will be useful if we choose url according to current language
+        myConfig = currConfig;
+        this.sizeMax = pSizeMax;
+        i18n = I18nFactory.getI18n("","lang/Messages",winLog.class.getClass().getClassLoader(),myConfig.getLocale(),0);    
+        this.htmlCode = pHTML;
+        showBrowser();
+    }    
+    
+    private void showBrowser() {
+        
+        Stage subStage = new Stage();
+        WebView wv = new WebView();
+        StackPane root = new StackPane();
+        root.getChildren().add(wv);
+
+        Scene scene = new Scene(root, 1000, 650);
+       
+        subStage.setScene(scene);        
+        subStage.show();
+        if (sizeMax) subStage.setMaximized(true);
+        
+        wv.getEngine().loadContent(htmlCode); 
+    }    
+}
