63e340d82dbd525df20821716137f665779ebf2e
gil <gil@mbp-de-gil-2.home>
2019-04-02 22:30:39 +0200

    Use the screen definition to resize the photo display

--- src/main/java/littlewins/winPhoto.java
+++ src/main/java/littlewins/winPhoto.java
@@ -7,11 +7,13 @@
 package littlewins;
 
 import javafx.animation.FadeTransition;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,11 +24,34 @@ import javafx.util.Duration;
  */
 public class winPhoto {
             
-    public void showDbPhoto(Image dbImage) {
+    public void displayPhoto(Image srcImage) {
         StackPane subRoot = new StackPane();
-        int winWidth = (int)dbImage.getWidth();
-        int winHeight = (int)dbImage.getHeight();
-        ImageView imgView = new ImageView(dbImage);
+        int winWidth = (int) srcImage.getWidth();
+        int winHeight = (int) srcImage.getHeight();
+        // Get screen size
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();        
+        double scrWidth = visualBounds.getWidth();
+        double scrHeight = visualBounds.getHeight();
+        double imgWidth = srcImage.getWidth();
+        double imgHeight = srcImage.getHeight();
+        
+        if (imgWidth > scrWidth) {
+            double scrRatio = scrWidth/imgWidth;
+            // keep a margin of 5%
+            scrRatio = scrRatio * 0.95;
+            winWidth = (int) (imgWidth * scrRatio);
+            winHeight = (int) (imgHeight * scrRatio);
+            if (winHeight > scrHeight) {
+                scrRatio = scrHeight/imgHeight;
+                // keep a margin of 5%
+                scrRatio = scrRatio * 0.95;
+                winWidth = (int) (imgWidth * scrRatio);
+                winHeight = (int) (imgHeight * scrRatio);
+            }
+        } 
+        ImageView imgView = new ImageView(srcImage);               
+        imgView.setFitWidth(winWidth);
+        imgView.setFitHeight(winHeight);
         subRoot.getChildren().add(imgView);       
         // Add a fading effect
         FadeTransition ft = new FadeTransition(Duration.millis(500), subRoot);
