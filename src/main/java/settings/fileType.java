a450cdba6917c5d33bfa59d9e691dd183141d778
gil <gil@mbp-de-gil-2.home>
2019-08-16 19:29:05 +0200

    Type file enumeration added

---
+++ src/main/java/settings/fileType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright Gil THOMAS
+ * This file forms an integral part of Logfly project
+ * See the LICENSE file distributed with source code
+ * for details of Logfly licence project
+ */
+package settings;
+
+/**
+ *
+ * @author gil
+ */
+    
+public enum fileType {IgcGpx,OpenAir,Gpx,Kml,OACheck,db,dbk,csv,wpt,xcp} 
+
