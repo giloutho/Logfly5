5a922e1c27179b03de0f96a512994e196a8024c6
gil <gil@mbp-de-gil-2.home>
2019-08-23 21:36:44 +0200

    Deprecated. The use of SerialPundit has been abandoned

--- src/main/java/gps/flytec15.java
+++ src/main/java/gps/flytec15.java
@@ -7,7 +7,13 @@
 package gps;
 
 /**
- *
+ * ==================================== DEPRECATED ===========================================
+ * Many problems with serialpundit on Windows 
+ * Many users have this error : com.serialpundit.core.SerialComException: 
+ *           C:\xx\spcomwinx64.dll: Can''t find dependent libraries
+ * Unable to solve this error with Visual C++ Redistributable For Visual Studio.
+ * ============================================================================================
+ * 
  * Communication with Flymaster SD series
  * @author Gil and Rishi Gupta https://github.com/RishiGupta12/SerialPundit
  * without Rishi help, this class doesn't exist
