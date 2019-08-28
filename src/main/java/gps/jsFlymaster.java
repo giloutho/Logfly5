09ee3fad233dcebc069086efa510bb66fd0809d5
gil <gil@mbp-de-gil-2.home>
2019-08-28 22:52:14 +0200

    Test with jSerialComm

--- src/main/java/gps/jsFlymaster.java
+++ src/main/java/gps/jsFlymaster.java
@@ -6,14 +6,14 @@
  */
 package gps;
 
+import com.fazecast.jSerialComm.SerialPort;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
-import jssc.SerialPort;
-import jssc.SerialPortException;
-import jssc.SerialPortTimeoutException;
 import systemio.mylogging;
 import waypio.pointRecord;
 
@@ -108,20 +108,36 @@ public class jsFlymaster {
     
     private void getListPFMWPL() throws Exception {
         
+        byte[] BufferRead = new byte[1048576];
+        int numRead;
+        int lenBuffer = 0;
+        
         try {
-            listPFMWP = new ArrayList<String>();
-            serialPort = new SerialPort(serialPortName);
-            serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_57600, 
-                                 SerialPort.DATABITS_8,
-                                 SerialPort.STOPBITS_1,
-                                 SerialPort.PARITY_NONE);              
-            serialPort.writeString("$PFMWPL,\n");
-            while(read_line()>0)
-            {                
-                listPFMWP.add(sbRead.toString());
-            }
-            serialPort.closePort();                
+            serialPort = SerialPort.getCommPort(serialPortName);
+            serialPort.openPort();//Open serial port
+            serialPort.setComPortParameters(57600, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);  
+            // TimeOut indispensable à priori
+            serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 500, 0);
+            String req = "$PFMWPL,\n";
+            byte[] b = req.getBytes(StandardCharsets.UTF_8); 
+            serialPort.writeBytes(b, b.length);
+            System.out.println("On a envoyé");
+            do
+            {
+               byte[] readBuffer = new byte[1024];
+               numRead = serialPort.readBytes(readBuffer, readBuffer.length);
+               System.arraycopy(readBuffer, 0, BufferRead,lenBuffer , numRead);
+                                 System.out.println("Read " + numRead + " bytes.");
+               lenBuffer += numRead; 
+            } while (numRead > 0);                      
+
+            serialPort.closePort();
+            
+            byte[] retval = new byte[lenBuffer];
+            System.arraycopy(BufferRead, 0, retval,0 , lenBuffer);
+            String rawWayp = new String(retval);
+            System.out.println(rawWayp);
+            listPFMWP = new ArrayList<>(Arrays.asList(rawWayp.split("\r\n")));
         } catch (Exception e) {
             sbError = new StringBuilder(this.getClass().getName()+"."+Thread.currentThread().getStackTrace()[1].getMethodName());
             sbError.append("\r\n").append(e.toString());
@@ -130,36 +146,23 @@ public class jsFlymaster {
     }   
     
     public void sendWaypoint() {
-        try {
-            serialPort = new SerialPort(serialPortName);
-            serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_57600, 
-                                     SerialPort.DATABITS_8,
-                                     SerialPort.STOPBITS_1,
-                                     SerialPort.PARITY_NONE);   
-            for (int i = 0; i < listPFMWP.size(); i++) {
-                write_line(listPFMWP.get(i));
-                // answer is read but not verification
-                read_line();
-            } 
-            serialPort.closePort();
-        } catch (Exception e) {
-            
-        }        
+//        try {
+//            serialPort = new SerialPort(serialPortName);
+//            serialPort.openPort();
+//            serialPort.setParams(SerialPort.BAUDRATE_57600, 
+//                                     SerialPort.DATABITS_8,
+//                                     SerialPort.STOPBITS_1,
+//                                     SerialPort.PARITY_NONE);   
+//            for (int i = 0; i < listPFMWP.size(); i++) {
+//                write_line(listPFMWP.get(i));
+//                // answer is read but not verification
+//                read_line();
+//            } 
+//            serialPort.closePort();
+//        } catch (Exception e) {
+//            
+//        }        
     }
-    
-    private void write_line(String reqDevice) {
-        String req;
-        
-        try {
-            req = reqDevice + "\n";
-            serialPort.writeString(reqDevice);
-        } catch (Exception e) {
-            sbError = new StringBuilder(this.getClass().getName()+"."+Thread.currentThread().getStackTrace()[1].getMethodName());
-            sbError.append("\r\n").append(e.toString());
-            mylogging.log(Level.SEVERE, sbError.toString());            
-        }
-    }   
      
     
     private int read_line() {
@@ -173,7 +176,7 @@ public class jsFlymaster {
             Thread.sleep(100);
             iLen = 0;
             while (iLen < iBufLen) {
-                iRes = serialPort.readBytes(1, 100);
+                serialPort.readBytes(iRes, 1);
                 if (iRes != null)  {
                     char cData = (char) (iRes[0] & 0xFF);
                         if (iRes[0] > 0 && cData != '\n') {
@@ -183,7 +186,7 @@ public class jsFlymaster {
                             }
                         } else {           
                             //  timed out
-                            //System.out.println("ilen : "+iLen+" "+sbRead.toString());
+                            System.out.println("ilen : "+iLen+" "+sbRead.toString());
                             break;
                         }
                 } else {
@@ -199,20 +202,5 @@ public class jsFlymaster {
         return iLen;
     }
     
-    private byte[] getData() throws SerialPortException, IOException {
-        
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] b;
-        sbRead = new StringBuilder();
-        
-        try {
-            while ((b = serialPort.readBytes(1, 100)) != null) {
-                baos.write(b);
-            }
-        } catch (SerialPortTimeoutException ex) {
-            ;   //don't want to catch it, it just means there is no more data         to read
-        }
-        
-        return baos.toByteArray();    
-    }       
+   
 }
