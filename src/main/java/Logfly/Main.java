a0dd9f5bb0801d5996c51282d7419ecdcb4b6897
gil <gil@mbp-de-gil-2.home>
2019-09-22 16:30:24 +0200

    beta mention added to the window title

--- src/main/java/Logfly/Main.java
+++ src/main/java/Logfly/Main.java
@@ -69,11 +69,11 @@ public class Main extends Application {
         // Current version
         Release release = new Release();
         release.setpkgver("5.0");        
-        release.setPkgrel("21");
+        release.setPkgrel("21");    // enlever beta en dessous
         // last bundle
         release.setseverity("5.21");
         
-        String currVersion = "Logfly "+release.getpkgver()+release.getPkgrel();
+        String currVersion = "Logfly "+release.getpkgver()+release.getPkgrel()+" beta 5.022a";
         this.primaryStage.setTitle(currVersion);
         
         // Reading settings
