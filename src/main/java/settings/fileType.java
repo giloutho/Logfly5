ef3c537cc7ecdba4119c2554f5346304f33c3044
gil <gil@mbp-de-gil-2.home>
2020-03-24 19:24:51 +0100

    Option GpxIgc added

--- src/main/java/settings/fileType.java
+++ src/main/java/settings/fileType.java
@@ -11,5 +11,5 @@ package settings;
  * @author gil
  */
     
-public enum fileType {IgcGpx,OpenAir,Gpx,Kml,OACheck,db,dbk,csv,wpt,xcp,foldertracks} 
+public enum fileType {IgcGpx,GpxIgc,OpenAir,Gpx,Kml,OACheck,db,dbk,csv,wpt,xcp,foldertracks} 
 
