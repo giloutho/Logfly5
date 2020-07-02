730fe397532fe5891a2475c3d2c725f59968ccf3
gil <gil@mbp-de-gil-2.home>
2020-07-02 13:06:32 +0200

    To keep header IGC, ExtractFirst function added

--- src/main/java/igc/mergingIGC.java
+++ src/main/java/igc/mergingIGC.java
@@ -15,6 +15,47 @@ public class mergingIGC {
     private StringBuilder sbTotRecB = new StringBuilder();
     
     /**
+     * Extraction of header and B Records from IGC original file
+     * IGC file was checked in logbook addition  
+     * @param sTrack 
+     */
+    public void extractFirst(String sTrack) {
+        String DebChar;
+        StringBuilder sbRec = new StringBuilder();
+        
+        // We received some tracklogs where there is only chr(10)
+        String BadCar = Character.toString((char)10);
+        String[] sLine = sTrack.split(BadCar);
+        int Lg_sLine =  sLine.length;
+        // later we received a track file with only chr(13) -:)
+        if (Lg_sLine == 1) {
+            BadCar = Character.toString((char)13);
+            sLine = sTrack.split(BadCar);
+            Lg_sLine = sLine.length;
+        }
+        if (Lg_sLine > 3) {     
+            for (int i = 0; i < Lg_sLine; i++) {
+                if (!sLine[i].trim().equals("")) {
+                    DebChar = sLine[i].substring(0,1);
+                    switch (DebChar) {
+                        case "A":
+                        case "H":
+                            sbRec.append(sLine[i]).append("\n");
+                            
+                            break;
+                        case "B" :
+                            if (DebChar.equals("B") && sLine[i].length() > 23) {
+                                sbRec.append(sLine[i]).append("\n");
+                            }
+                            break;
+                    }
+                }                                     
+            }
+        }        
+        sbTotRecB.append(sbRec.toString());
+    }    
+    
+    /**
      * Extraction of B Records from IGC original file
      * IGC file was checked in logbook addition  
      * @param sTrack 
@@ -38,7 +79,7 @@ public class mergingIGC {
                 if (!sLine[i].trim().equals("")) {
                     DebChar = sLine[i].substring(0,1);
                     if (DebChar.equals("B") && sLine[i].length() > 23) {
-                        sbRecB.append(sLine[i]);
+                        sbRecB.append(sLine[i]).append("\n");
                     }
                 }                                     
             }
