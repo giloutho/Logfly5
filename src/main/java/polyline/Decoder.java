3aebb1da1b20506dd66df2b2e013140e9140e988
gil <gil@mbp-de-gil-2.home>
2020-07-17 16:10:45 +0200

    Polyline decoding, initial commit

---
+++ src/main/java/polyline/Decoder.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Gil THOMAS
+ * This file forms an integral part of Logfly project
+ * See the LICENSE file distributed with source code
+ * for details of Logfly licence project
+ */
+package polyline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Found on https://github.com/scoutant/polyline-decoder
+ * 
+ * Port to Java of Mark McClures Javascript PolylineEncoder :
+ * http://facstaff.unca.edu/mcmcclur/GoogleMaps/EncodePolyline/decode.js
+ */
+
+public class Decoder {
+    
+    private static final double DEFAULT_PRECISION = 1E5;
+
+    public List<Point> decode(String encoded) {
+        return decode(encoded, DEFAULT_PRECISION);
+    }
+
+    /**
+     * Precision should be something like 1E5 or 1E6. For OSRM routes found precision was 1E6, not the original default
+     * 1E5.
+     *
+     * @param encoded
+     * @param precision
+     * @return
+     */
+    public List<Point> decode(String encoded, double precision) {
+        List<Point> track = new ArrayList<Point>();
+        int index = 0;
+        int lat = 0, lng = 0;
+
+        while (index < encoded.length()) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            Point p = new Point((double) lat / precision, (double) lng / precision);
+            track.add(p);
+        }
+        return track;
+    }
+    
+}
+
